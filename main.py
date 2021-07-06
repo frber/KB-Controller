97c4d8a9a18f9ac7483cf70d6ec736a1423c12a3
frber <78807896+frber@users.noreply.github.com>
2021-07-06 15:56:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,12 +21,12 @@ class Gui:
 
         self.master = master
         self.master.title("KBC 1.1")
-        self.master.geometry("900x500")
+        self.master.geometry("700x350")
 
         # Skapar tabs (använder ej flera tabs, men kan vara bra att ha framöver).
         self.tabcontrol = Notebook(master)
         self.tab1 = Frame(self.tabcontrol)
-        self.tabcontrol.add(self.tab1, text="Kontroll")
+        self.tabcontrol.add(self.tab1, text="fredrik.bergstrom@ltu.se")
         self.tabcontrol.pack(expand=1, fill="both")
 
         # Ikon
@@ -38,16 +38,32 @@ class Gui:
         self.label_ltu_logga.place(y=40, x=40, anchor="center")
 
         # Knapp för välj mapp
-        self.knapp_valj = Button(self.tab1, text="Välj mapp för e-bokslut", width=25, command=self.valj_filvag)
-        self.knapp_valj.place(y=100, x=200, anchor="center")
+        self.filvag_ebokslut = "tom"
+        self.label_vald_filvag = Label(self.tab1, text="")
+        self.label_vald_filvag.place(y=270, x=350, anchor="center")
+        self.knapp_valj = Button(self.tab1, text="1. Välj mapp för e-bokslut", width=25, command=self.valj_filvag)
+        self.knapp_valj.place(y=100, x=350, anchor="center")
 
         # Knapp Starta
-        self.knapp_starta = Button(self.tab1, text="Starta", width=25, command=self.thread)
-        self.knapp_starta.place(y=140, x=200, anchor="center")
+        self.knapp_starta = Button(self.tab1, text="2. Starta", width=25, command=self.thread)
+        self.knapp_starta.place(y=140, x=350, anchor="center")
 
         # Progressbar
-        self.prog_bar = Progressbar(self.tab1, style="blue.Horizontal.TProgressbar", orient=HORIZONTAL, length=600, maximum=100, mode='determinate')
-        self.prog_bar.place(y=400, x=200)
+        self.prog_bar = Progressbar(self.tab1, orient=HORIZONTAL, length=600, maximum=100, mode='determinate')
+        self.prog_bar.place(y=250, x=350, anchor="center")
+
+        # Label kontrollerad fil
+        self.label_kontrollerad_fil = Label(self.tab1, text="", font= (20))
+        self.label_kontrollerad_fil.place(y=230, x=350, anchor="center")
+
+        # Label antal kontrollerade filare
+        self.label_antal_fil = Label(self.tab1, text="Kontrollerade berpers: 0")
+        self.label_antal_fil.place(y=10, x=615, anchor="center")
+
+        # Rubrik
+        self.label_rubrik = Label(self.tab1, text="Kontroll av berper", font=('Helvetica', 20, 'bold'))
+        self.label_rubrik.place(y=50, x=350, anchor="center")
+
 
 
 
@@ -57,11 +73,11 @@ class Gui:
         # Tilldelar variabel vald filväg och uppdaterar label.
         # Satte Stringvar() som self. pga att metoden är knuten till en knapp och filvägen behövs i andra metoder.
         self.filvag_ebokslut = StringVar()
-        vald_filvag = filedialog.askdirectory()
-        self.filvag_ebokslut.set(vald_filvag)
-        label_vald_filvag = Label(self.tab1)
-        label_vald_filvag["text"] = vald_filvag
-        label_vald_filvag.place(y=200, x=300)
+        self.vald_filvag = filedialog.askdirectory()
+        self.filvag_ebokslut.set(self.vald_filvag)
+        #label_vald_filvag = Label(self.tab1)
+        self.label_vald_filvag["text"] = self.vald_filvag
+
 
     def thread(self):
         # Använder en annan thread så att gränssnittet inte fryser medans huvudprogrammet körs.
@@ -70,12 +86,16 @@ class Gui:
         t.start()
 
     def starta(self):
-        self.prog_bar.start(1)
-        iterera = ItereraMappFil(self.filvag_ebokslut)
-        self.prog_bar.stop()
+
+        if self.filvag_ebokslut == "tom":
+            messagebox.showerror("OBS!", "Du behöver välja en filväg innan du börjar.")
+        else:
+            self.prog_bar.start(1)
+            iterera = ItereraMappFil(self.filvag_ebokslut, self.label_kontrollerad_fil, self.label_antal_fil)
+            self.prog_bar.stop()
 
 def main():
-    master = ThemedTk(theme="blue")
+    master = ThemedTk(theme="arc")
     gui = Gui(master)
     master.mainloop()
 
